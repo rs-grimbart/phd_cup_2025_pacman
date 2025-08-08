6298a8ca13a8547843f5c0e620cc3b412af23bf8
Jakob Strutz <jakob.strutz@infineon.com>
2025-08-08 17:07:16 +0200

    Speed top of script

--- Pacman/pacman.py
+++ Pacman/pacman.py
@@ -11,6 +11,7 @@ import threading
 SERIAL_PORT = sys.argv[1] if len(sys.argv) > 1 else "COM21"  # Default to COM15 if no argument is provided
 BAUD_RATE = 115200 ## You don't need to change this
 
+speedlimit = 3.0
   
 black = (0,0,0)
 white = (255,255,255)
@@ -252,7 +253,6 @@ class Ghost(Player):
     # Change the speed of the ghost
     def changespeed(self,list,ghost,turn,steps,l):
       try:
-        speedlimit = 0.75
         z=list[turn][2]
         if steps < z:
           self.change_x=list[turn][0] * speedlimit
